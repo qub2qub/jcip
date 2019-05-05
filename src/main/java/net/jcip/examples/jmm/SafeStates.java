8b12d018b90cae8c5480cdf95854247d7b2daacb
qub2qub <email@example.com>
2019-05-05 20:16:03 +0300

    jcip ch_16_mid

--- src/main/java/net/jcip/examples/jmm/SafeStates.java
+++ src/main/java/net/jcip/examples/jmm/SafeStates.java
@@ -5,14 +5,21 @@ import java.util.*;
 import net.jcip.annotations.*;
 
 /**
- * Initialization safety for immutable objects
+ * Initialization safety for immutable objects:
+ * SafeStates could be safely published even through unsafe lazy initialization or stashing a reference
+ * to a SafeStates in a public static field with no synchronization,
+ * even though it uses no synchronization and relies on the non-thread-safe HashSet.
  */
 @ThreadSafe
 public class SafeStates {
+    /**
+     * However, a number of small changes to SafeStates would take away its thread safety.
+     * If states were not final, or if any method other than the constructor modified its contents
+     */
     private final Map<String, String> states;
 
     public SafeStates() {
-        states = new HashMap<String, String>();
+        states = new HashMap<>();
         states.put("alaska", "AK");
         states.put("alabama", "AL");
         /*...*/
